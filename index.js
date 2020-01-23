ea7780952fea9adf5011294d6d311c98608290dd
aSedali <toseyyedali@gmail.com>
2020-01-23 10:50:01 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
   
 const express = require('express')
 const app = express()
+var request = require('request');
 const port = process.env.PORT || 3131
 const screenshot = require('./screenshot')
 screenshot('https://tux2nux.blogspot.com')
 var minutes = 10, the_interval = minutes * 60 * 1000;
 setInterval(function() {
-  console.log("I am doing my 5 minutes check");
+  console.log("I am doing my 10 minutes check");
   // do your stuff here
   screenshot('https://tux2nux.blogspot.com')
 }, the_interval);
@@ -22,4 +23,16 @@ app.get('/screenshot', (req, res) => {
   })()
 })
 
-app.listen(port, () => console.log(`app listening on port ${port}!`))
+app.listen(port, () => )
+const server = app.listen(port, () => {
+    const host = server.address().address
+    const port = server.address().port
+    console.log(`app listening on port ${port}!`)
+    console.log("Example app listening at http://%s:%s", host, port)
+  setInterval(function() {
+  console.log("I am doing my 10 minutes check");
+  // do your stuff here
+    request("http://%s:%s", host, port)
+  screenshot('https://tux2nux.blogspot.com')
+}, the_interval);
+});
