f752ed2b5fd734d169640b0943db1d55d334f030
aSedali <toseyyedali@gmail.com>
2020-01-26 11:49:25 +0330

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,5 +1,28 @@
+const puppeteer = require('puppeteer')
+const screenshot = require('./screenshot')
+
+module.exports = function (url) {
+  return new Promise((resolve, reject) => {
+    ;(async () => {
+      const browser = await puppeteer.launch({
+        args: ['--no-sandbox']
+      })
+
+      const page = await browser.newPage()
+
+      await page.goto(url, {
+        waitUntil: ['load', 'networkidle0', 'domcontentloaded']
+      })
+  console.log("on tux2nux ...");
+    //  await page.waitFor(1000000000)
+
+
+     // await browser.close()
+
+    })()
+  })
+}
+
 console.log("Worker starting...");
 
-setInterval(() => {
-  console.log("Worker still running.");
-}, 1000);
+screenshot('https://tux2nux.blogspot.com')  
