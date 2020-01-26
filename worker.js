ba11df90eec1b8897e683b55edd5b4a5441349e1
aSedali <toseyyedali@gmail.com>
2020-01-26 14:51:08 +0330

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,20 +1,22 @@
+
+
 const puppeteer = require('puppeteer');
 console.log("Worker starting...");
-var url='https://tux2nux.blogspot.com';
-;(async () => {
+(async () => {
       const browser = await puppeteer.launch({
         args: ['--no-sandbox']
       });
-
-      const page = await browser.newPage();
+  const page = await browser.newPage();
   console.log("go to tux2nux ...");
-      await page.goto(url, {
-        waitUntil: ['load', 'networkidle0', 'domcontentloaded']
-      });
+  await page.goto('https://tux2nux.blogspot.com');
   console.log("on tux2nux ...");
+  const textContent = await page.evaluate(() => document.querySelector('p').textContent);
+  const innerText = await page.evaluate(() => document.querySelector('p').innerText);
+
+  console.log(textContent);
+  console.log(innerText);
+
      await page.waitFor(1000000000);
   console.log("out of tux2nux ...");
-
-     // await browser.close();
-
-    })();
+  browser.close();
+})();
