439299437602853e93f3cc7b19265e9d26f0b5fb
aSedali <toseyyedali@gmail.com>
2020-01-27 10:48:22 +0330

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,5 +1,3 @@
-
-
 const puppeteer = require('puppeteer');
 console.log("Worker starting...");
 (async () => {
@@ -10,12 +8,9 @@ console.log("Worker starting...");
   console.log("go to tux2nux ...");
   await page.goto('https://tux2nux.blogspot.com');
   console.log("on tux2nux ...");
-  setInterval(function() {
-      const innerText = await page.evaluate(() => document.querySelector('p').innerText);
+  const innerText = await page.evaluate(() => document.querySelector('p').innerText);
   console.log(innerText);
-}, 60 * 1000);
-
- await page.waitFor(1000000000);
+  await page.waitFor(1000000000);
   console.log("out of tux2nux ...");
   browser.close();
 })();
