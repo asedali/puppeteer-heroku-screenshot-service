3ffee79400120c29e6760df0e832a8676eed6228
aSedali <toseyyedali@gmail.com>
2020-02-09 15:33:44 +0330

    Update and rename worker.js to work.js

--- work.js
+++ work.js
@@ -1,22 +1,19 @@
 const puppeteer = require('puppeteer');
 console.log("Worker starting...");
-setInterval(function() {
-      console.log("I am UP...");
-},10000);
+
 (async () => {
       const browser = await puppeteer.launch({
         args: ['--no-sandbox']
       });
       
   const page = await browser.newPage();
-  console.log("go to tux2nux ...");
+  console.log("go to newScript ...");
       
       
       page.setDefaultNavigationTimeout(0);
       page.setDefaultTimeout(0);
-      
-      
-  await page.goto('https://tux2nux.blogspot.com');
+
+  await page.goto('https://nimiqintest.blogspot.com/');
   console.log("on tux2nux ...");
       for (;;) {
   const innerText = await page.evaluate(() => document.querySelector('p').innerText);
@@ -24,6 +21,6 @@ setInterval(function() {
             await page.waitFor(10000);
             }
  // await page.waitFor(1000000000);
-  console.log("out of tux2nux ...");
+  console.log("out of new ...");
  // browser.close();
 })();
